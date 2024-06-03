dc2cc5430952ab32f1d5db38c48c82f508387190
C-r0 <146779565+C-r0@users.noreply.github.com>
2024-06-02 21:14:12 -0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,4 +1,21 @@
 RegisterCommand("id", function() 
+lib.getClosestPlayer(coords, maxDistance, includePlayer)
+lib.notify({
+    title = 'ID do player mais proximo.',
+    description = playerId,
+    showDuration = true,
+    position = 'top-right',
+    style = {
+        backgroundColor = '#141517',
+        color = '#C1C2C5',
+        ['.description'] = {
+          color = '#909296'
+        }
+    },
+    icon = 'id-card'
+})
+end)
+RegisterCommand("meuid", function()
 lib.notify({
     title = 'Seu ID',
     description = '*'..GetPlayerServerId(PlayerId())..'*',
